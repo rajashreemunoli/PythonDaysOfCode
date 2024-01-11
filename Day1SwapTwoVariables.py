9af94ce314730439b77cc04a94292ed050ef4fec
rajashreemunoli <52010135+rajashreemunoli@users.noreply.github.com>
2024-01-11 15:26:51 +0100

    Add files via upload

---
+++ Day1SwapTwoVariables.py
@@ -0,0 +1,13 @@
+#Create a program that swaps the values of two variables
+a = input('Enter value of a: ')
+b = input('Enter value of b: ')
+
+print("Before Swapping:")
+print("a =", a)
+print("b =", b)
+
+a,b=b,a
+
+print("After Swapping:")
+print("a =", a)
+print("b =", b)
