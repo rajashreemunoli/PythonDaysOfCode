42f8489ebffdcf1ee4a3a1adc7bcd4fbc7d1b7d0
rajashreemunoli <52010135+rajashreemunoli@users.noreply.github.com>
2024-02-03 19:23:32 +0100

    Update to Day22FindSecondLargestElementInAList.py
    Updated code to handle instances where there are repetitions of numbers in a list

--- Day22FindSecondLargestElementInAList.py
+++ Day22FindSecondLargestElementInAList.py
@@ -1,4 +1,9 @@
 #Create a program to find the second-largest element in a list
+def second_largest(lst1):
+	lst2=list(set(lst1))
+	lst2.sort()
+	return lst2[-2]
+
 
 print("Enter list elements one by one. Press enter when done.\n")
 inputlist=[]
@@ -8,6 +13,6 @@ try:
 		inputlist.append(float(x))
 		x=input()
 
-	print("Second largest element in list is:",sorted(inputlist)[-2])
+	print("Second largest element in list is:",second_largest(inputlist))
 except:
 	print('Enter numbers only')
