c1cf5ae4bb080c70d636becca8925204f0eeb573
rajashreemunoli <52010135+rajashreemunoli@users.noreply.github.com>
2024-02-04 15:17:33 +0100

    Add files via upload

---
+++ Day25ConcatenateTwoLists.py
@@ -0,0 +1,5 @@
+#Create a program to concatenate two lists
+l1=input("Enter first list elements separated by comma\n").split(',')
+l2=input("Enter second list elements separated by comma\n").split(',')
+l3=l1+l2
+print("Concatenated list: ",l3)
