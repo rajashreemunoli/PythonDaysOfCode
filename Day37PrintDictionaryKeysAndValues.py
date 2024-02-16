b82678fb77f219df6367017c0ec4a0361ee3dd74
rajashreemunoli <52010135+rajashreemunoli@users.noreply.github.com>
2024-02-16 01:10:56 +0100

    Add files via upload

---
+++ Day37PrintDictionaryKeysAndValues.py
@@ -0,0 +1,11 @@
+#Write a program to iterate through a dictionary and print its keys and values
+d= {
+  "a":1,
+  "b":2,
+  "c":3,
+  "d":4,
+  "e":5
+}
+
+for key,value in d.items():
+	print(key,value)
