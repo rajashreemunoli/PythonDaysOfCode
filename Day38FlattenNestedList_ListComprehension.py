49f062c212f88245409815b0ab4e7d2237168b16
rajashreemunoli <52010135+rajashreemunoli@users.noreply.github.com>
2024-02-17 17:19:47 +0100

    Delete Day38FlattenNestedList_ListComprehension.py

--- Day38FlattenNestedList_ListComprehension.py
+++
@@ -1,9 +0,0 @@
-#Write a program to flatten a nested list
-def flat_listcompr(l):
-	flist=[element for innerList in l for element in innerList]
-	return flist
-
-input_list=["WWC",["Python","Challenge"],[2024,[17,2,2024]],['a','b','c'],[1,2,3]]
-
-print("Nested List: ",input_list)
-print("\nFlattened List: ",flat_listcompr(input_list))
