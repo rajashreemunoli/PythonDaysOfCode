302478e5174bb61c72e4a955e7b6cf384b06f7f6
rajashreemunoli <52010135+rajashreemunoli@users.noreply.github.com>
2024-02-20 12:09:57 +0100

    Add files via upload

---
+++ Day41UseRecursionGenerateAllPermutationsOfList.py
@@ -0,0 +1,12 @@
+#Write a program that uses recursion to generate all permutations of a list
+def permute_list(lst,f=0):
+	#base case
+	if f>=len(lst):
+		print(lst)
+
+	for s in range(f,len(lst)):
+		lst[f],lst[s]=lst[s],lst[f]
+		permute_list(lst,f+1)
+		
+
+permute_list([1,2,3])
